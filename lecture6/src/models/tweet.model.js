725c6de07494a245dffd11155a8d1a2d02ef4b80
Smeet-555 <smeetr2006@gmail.com>
2025-10-22 19:50:46 +0530

    feat: add models for comments, likes, playlists, and tweets with pagination support

---
+++ lecture6/src/models/tweet.model.js
@@ -0,0 +1,10 @@
+import mongoose ,{Schema} from "mongoose";
+import mongooseAggregatePaginate  from "mongoose-aggregate-paginate-v2";
+
+
+const TweetSchema = new Schema({
+    content: {type: String, required: true},
+    owner: {type: Schema.Types.ObjectId, ref: "User" },
+} , {timestamps: true})
+
+export const Tweet = mongoose.model("Tweet", TweetSchema );
