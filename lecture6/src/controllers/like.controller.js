aacdea08793dba7605ced31257dcbc04f271544c
Smeet-555 <smeetr2006@gmail.com>
2025-10-25 18:10:28 +0530

    like controller and subscription controller created

--- lecture6/src/controllers/like.controller.js
+++ lecture6/src/controllers/like.controller.js
@@ -4,26 +4,75 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
+// creating a like toggler for any model
+const toggleLike = asyncHandler(async ({userId , targetId , targetType}) =>{
+    if(!isValidObjectId(targetId)){
+        throw new ApiError (400 , "Invalid id format")
+    }
+    const existingLike = await Like.findOne({
+        likedBy : userId , 
+        [`${targetType}`] : targetId
+    })
+
+    if(existingLike){
+        await existingLike.deleteOne();
+        return {liked : false}
+    }
+    else{
+        await Like.create({
+            likedBy : userId,
+            [`${targetType}`] : targetId
+        });
+        return {liked : true}
+    }
+})
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
-    //TODO: toggle like on video
+    const userId = req.user?._id
+
+    const videoLike = asyncHandler(async ({userId , targetId : "videoid" , targetType : "video"}))
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200 , videoLike , `Video ${videoLike.liked? "liked" : "unliked"} successfully`))
+
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
-    //TODO: toggle like on comment
+    const userId = req.user?._id
+
+    const commentLike = asyncHandler(async ({userId , targetId : "commentId" , targetType : "comment"}))
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200 , commentLike , `Comment ${commentLike.liked? "liked" : "unliked"} successfully`))
+
 
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
-    //TODO: toggle like on tweet
-}
-)
+    const userId = req.user?._id;
+
+    const tweetLike = await toggleLike({ userId, targetId: tweetId, targetType: "tweet" });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, tweetLike, `Tweet ${tweetLike.liked ? "liked" : "unliked"} successfully`));
+});
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
-})
+    const userId = req.user?._id;
+
+    const likedVideos = await Like.find({ likedBy: userId, video: { $exists: true } })
+        .populate("video");
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, likedVideos, "Fetched all liked videos successfully"));
+});
+
 
 export {
     toggleCommentLike,
--- lecture6/src/controllers/subscription.controller.js
+++ lecture6/src/controllers/subscription.controller.js
@@ -1,28 +1,78 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {User} from "../models/user.model.js"
-import { Subscription } from "../models/subscription.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose";
+import { User } from "../models/user.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-
+// create a subscription toggler
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-    // TODO: toggle subscription
-})
+    const { channelId } = req.params;
+    const subscriberId = req.user?._id;
 
-// controller to return subscriber list of a channel
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channel ID");
+    }
+
+    if (subscriberId.toString() === channelId.toString()) {
+        throw new ApiError(400, "You cannot subscribe to your own channel");
+    }
+
+    const existingSubscription = await Subscription.findOne({
+        subscriber: subscriberId,
+        channel: channelId
+    });
+
+    if (existingSubscription) {
+        await existingSubscription.deleteOne();
+        return res
+            .status(200)
+            .json(new ApiResponse(200, { subscribed: false }, "Unsubscribed successfully"));
+    } else {
+        await Subscription.create({
+            subscriber: subscriberId,
+            channel: channelId
+        });
+        return res
+            .status(200)
+            .json(new ApiResponse(200, { subscribed: true }, "Subscribed successfully"));
+    }
+});
+
+
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-})
+    const { channelId } = req.params;
 
-// controller to return channel list to which user has subscribed
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channel ID");
+    }
+
+    const subscribers = await Subscription.find({ channel: channelId })
+        .populate("subscriber", "username email avatar");
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, subscribers, "Fetched all subscribers successfully"));
+});
+
+
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
-})
+    const { subscriberId } = req.params;
+
+    if (!isValidObjectId(subscriberId)) {
+        throw new ApiError(400, "Invalid subscriber ID");
+    }
+
+    const subscribedChannels = await Subscription.find({ subscriber: subscriberId })
+        .populate("channel", "username email avatar");
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, subscribedChannels, "Fetched subscribed channels successfully"));
+});
 
 export {
     toggleSubscription,
     getUserChannelSubscribers,
     getSubscribedChannels
-}
+};
